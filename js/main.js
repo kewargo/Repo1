573ca478ea740e005d0d9715b9c6e1816c6ddd61
Kerry Wargo <kewargo@verizon.net>
2013-10-09 20:34:01 -0400

    Old McDonald & Fizz Buzz

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-var myButton = document.getElementById("clickButton");
-var myText = document.getElementById("helloText");
+var animal = prompt("Name your favorite animal", " ");
+var sound = prompt("What sound does your animal make?", " ");
 
-myButton.addEventListener('click', doSomething, false);
 
-function doSomething() {
-	myText.textContent = "Hello, World! From Cloud9";
-}
+document.write("Old McDonald had a farm, E I E I Ooooo <br>");
+document.write("and on this farm he had a " + animal + " E I E I Oooo <br>");
+document.write("with a " + sound + " " + sound + " here <br>");
+document.write("and a " + sound + " " + sound + " there <br>");
+document.write("here a " + sound + " there a " + sound + "<br>");
+document.write("everywhere a " + sound + " " + sound + "<br>");
+document.write("Old McDonald had a farm, E I E I Ooooo <br>");
---
+++ js/main2.js
@@ -0,0 +1,23 @@
+
+
+// Variable Declarations for Fizz Buzz
+var i = 0;
+var fizzResult = 0;
+var buzzResult = 0;
+
+// For loop for Fizz Buzz
+for(i=1; i<=100; i++) 
+{
+    fizzResult = (i % 3); 
+    buzzResult = (i % 5);
+    
+    if (fizzResult === 0 && buzzResult === 0) {document.write("FizzBuzz!!! <br>")}
+
+        else if (fizzResult === 0) {document.write("Fizz! <br>")}
+            
+            else if (buzzResult === 0) {document.write("Buzz! <br>")}
+            
+                else (document.write(i + "<br>"))
+    
+}
+
